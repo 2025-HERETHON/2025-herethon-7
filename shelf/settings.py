339b2bc8e7d863f9a62a07339954b986143847d5
yxpjseo <yjins2004@gmail.com>
2025-07-01 14:54:52 +0900

    백엔드 변경사항 반영 및 base.html, base.css 수정

--- shelf/settings.py
+++ shelf/settings.py
@@ -123,6 +123,7 @@ LOGIN_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+from dotenv import load_dotenv
+import os
 load_dotenv()
-
 GOOGLE_BOOKS_API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY")
