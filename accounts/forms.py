8991f2d0a3f9dbe35cce314d490705e0a46e002a
xeeone <xeeone@Jiwon-MacBook-Pro.local>
2025-07-03 02:23:28 +0900

    회원가입 페이지 css 작성

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,16 +9,48 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
         self.fields['user_id'].label = '아이디'
+        self.fields['user_id'].widget.attrs.update({
+            'placeholder': '숫자, 영문 조합 8자 이상',
+            'class': 'form-input'
+        })
+
         self.fields['nickname'].label = '닉네임'
+        self.fields['nickname'].widget.attrs.update({
+            'placeholder': '',
+            'class': 'form-input'
+        })
+
         self.fields['gender'].label = '성별'
+        self.fields['gender'].widget.attrs.update({
+            'class': 'form-input'
+        })
+
         self.fields['email'].label = '이메일'
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'example@email.com',
+            'class': 'form-input'
+        })
+
         self.fields['password1'].label = '비밀번호'
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': '숫자, 영문 조합 8자 이상',
+            'class': 'form-input'
+        })
+
         self.fields['password2'].label = '비밀번호 확인'
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': '비밀번호 재입력',
+            'class': 'form-input'
+        })
 
 class CustomLoginForm(AuthenticationForm):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': '아이디'})
+        widget=forms.TextInput(attrs={
+            'placeholder': '아이디',
+            'class': 'form-input'
+        })
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': '비밀번호'})
