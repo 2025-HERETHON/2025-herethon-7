2ffed091a58c0e101c2479fe7012695e00875e9d
xeeone <xeeone@Jiwon-MacBook-Pro.local>
2025-07-05 00:49:35 +0900

    리뷰창 css 추가

--- contents/urls.py
+++ contents/urls.py
@@ -5,6 +5,7 @@ app_name = 'contents'
 
 urlpatterns = [
     path('', main, name='main'),
+    path('create/', create, name='create_default'),
     path('create/<int:book_id>/', create, name='create'),
     path('books/search/', book_search, name='book_search'),
     path('books/select/', select_book, name='select_book'),
