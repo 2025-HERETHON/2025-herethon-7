973769edd7c7e975208c324e2ad99762999c4281
yxpjseo <yjins2004@gmail.com>
2025-07-11 22:15:31 +0900

    Stop tracking shelf/__init__.py

--- shelf/__init__.py
+++
@@ -1,7 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-from django.core.files.storage import default_storage
-default_storage._wrapped = S3Boto3Storage()
-
-#accounts/__init__.py
-import pymysql
-pymysql.install_as_MySQLdb()
